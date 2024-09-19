d2a55804bbe3dea608204b245ed20c24056812f4
Yenduriraghumadhav <raghumadhav2002@gmail.com>
2024-09-20 00:28:19 +0530

    raghu

---
+++ my 6 python.py
@@ -0,0 +1,5 @@
+name1 = input("my name is madhav")
+name2 =input(" what is you name? ")
+age1 = input("my age is 40")
+age2 = int(input("whats your age"))
+print("my name", name2, "age ",age2 ,"thank you")

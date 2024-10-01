48efd154389e1e483e54cb5cad8c75dcc64424d6
Yenduriraghumadhav <raghumadhav2002@gmail.com>
2024-10-02 00:40:55 +0530

    add

---
+++ my python 8.py
@@ -0,0 +1,6 @@
+name1 = input("My name is Madhav: ")
+name2 = input("What is your name? ")
+age1 = input("My age is 40: ")
+age2 = int(input("What's your age? "))
+
+print("My name is", name2, "age is", age2, "thank you.")
---
+++ my python9 .py
@@ -0,0 +1,4 @@
+sum1=20
+sum2=20
+sum3=sum1+sum2
+print(sum3)
